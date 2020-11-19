174b529e6c7b0bb2c4de8881341077d1518646b2
stasiya.issayeva <stasiy.issayeva@gmail.com>
2020-11-19 09:29:37 +0600

    + Add files

---
+++ 1_week/lesson_3/src/Square.java
@@ -0,0 +1,36 @@
+
+
+public class Square extends Rectangle{
+	Square(){
+		super();
+	}
+	
+	Square(double side){
+		super(side, side);
+	}
+	
+	Square(double side, String color, boolean filled){
+		super(side, side, color, filled);
+	}
+	
+	public double getSide(){
+		return super.getLength();
+	}
+	
+	public void setLength(double length) {
+		super.setLength(length);
+	}
+	
+	public void setWidth(double width) {
+		super.setWidth(width);
+	}
+	
+	public void setSide(double side) {
+		this.setLength(side);
+		this.setWidth(side);
+	}
+	
+	public String toString() {
+		return "Square[" + super.toString() + "]";
+	}
+}
